b7bfe37229ca59981e0a3ae5d5cd3b2be562b084
mscarce <oluwa14tobi@gmail.com>
2023-12-05 16:39:31 +0100

    api calls test

--- script.js
+++ script.js
@@ -9,31 +9,46 @@ function changePage(e){
 
 const registerUser = (password, mail) =>{
     const registrationData = {
-        email: mail,
-        password: password
+        "email": mail,
+        "password": password
       };
-      console.log(registrationData.password);
-    console.log(registrationData.email);
+
+      console.log(password, mail)
+
+      let xhr = new XMLHttpRequest()
+      xhr.open('POST', "https://87f1-105-112-75-119.ngrok.io/recipe-management/register")
+
+      xhr.setRequestHeader('Content-Type', 'application/json')
       
-      fetch('https://dc51-62-173-45-70.ngrok.io/recipe-management/register', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(registrationData)
-      })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-          return response.json();
-        })
-        .then(data => {
-            // this.location.href = 'menu/menu.html';
-            console.log('Registration Successful:', data);
-        })
-        .catch(error => {
-          console.error('Error:', error);
-        });
+      xhr.send({
+        "email": `${mail}`,
+        "password": `${password}`
+      });
+
+      xhr.load = function(){
+        console.log(xhr.status)
+      }
+
+    //   fetch('https://1128-62-173-45-70.ngrok.io/recipe-management/register', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json'
+    //     },
+    //     mode: 'no-cors',
+    //     body: registrationData
+    //   })
+    //     .then(response => {
+    //       if (!response.ok) {
+    //         throw new Error(`HTTP error! Status: ${response.status}`);
+    //       }
+    //       return response.json();
+    //     })
+    //     .then(data => {
+    //         // this.location.href = 'menu/menu.html';
+    //         console.log('Registration Successful:', data);
+    //     })
+    //     .catch(error => {
+    //       console.error('Error:', error);
+    //     });
       
 }
