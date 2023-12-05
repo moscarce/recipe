3b3b4913c5a340230fe9e572497dda014cb1d253
mscarce <oluwa14tobi@gmail.com>
2023-12-05 08:53:47 +0100

    responsive done

--- menu.js
+++ menu.js
@@ -11,7 +11,7 @@ food.innerHTML = 'Beans';
 let recipeIngredient = document.createElement('p')
 recipeIngredient.innerHTML = 'Ingredients:<span><i> ewa, ponmo</i></span>';
 let instruction = document.createElement('p') 
-instruction.innerHTML =  'Instruction:<span><i> its working ........................................................................................................................................................</i></span>';
+instruction.innerHTML =  'Instruction:<span><i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ex suscipit eligendi voluptates odio excepturi alias omnis. Corrupti repellendus perferendis, unde architecto impedit inventore labore id harum atque nulla velit</i></span>';
 let hr = document.createElement('hr');
 
     recipe.appendChild(food);
